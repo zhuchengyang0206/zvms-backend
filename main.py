031fba14eef223a0d8834d9e17885db1ae900637
朱程炀 <zhuchengyangclever@163.com>
2020-09-19 18:11:32 +0800

    update main.py.

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-# from flask import Flask
-import socket # ?
-import json
-import httplib
+from flask import Flask
 import sqlite3
-
-# Flask init
-# app = Flask(__name__)
-
-# socket init
-s = socket().socket()
-host = socket.gethostname()
-port = 8000
-s.bind((host, port))
 
 # SQL init
 conn = sqlite3.connect('users')
 c = conn.cursor()
 
-# start
-s.listen(1) # max connection
-cli, addr = s.accept()
-while True:
-    msg = cli.recv(1024)
+# Flask init
+app = Flask(__name__)
+
+@app.route('/')
+def analyzer():
+    return 'test'
+
+app.run()
