854a6705ac32cc1b47fb44903434cd633b2ef4e4
朱程炀 <zhuchengyangclever@163.com>
2020-11-08 15:56:23 +0800

    今天没时间改了，已经改的当作个示范8

--- oppressor.py
+++ oppressor.py
@@ -1,6 +1,44 @@
 # 随手打的一个词竟然还真的有，那就用这个名吧
 # 这个文件封装了一些针对数据库的操作
 import database as DB
+
+thisYear = 2020 # 以后要改成自动获取
+
+def classIdToString(a):
+    global thisYear
+    id = int(a)
+    _year = id // 100
+    _class = id % 100
+    ret = ""
+    # 特殊身份的判断
+    # 教师 100001 100002
+    # 管理员 110001 110002
+    # 系统 110003 110004
+    if _year//100 == 10:
+        ret = "教师"
+        return ret
+    elif _year//100 == 1:
+        ret = "管理员"
+        return ret
+    elif _year//100 == 12:
+        ret = "系统"
+        return ret
+    
+    if _class <= 10:
+        ret = ret + "高"
+    elif _class <= 17:
+        ret = ret + "蛟"
+    if _year == thisYear:
+        ret = ret + "一"
+    elif _year == thisYear - 1:
+        ret = ret + "二"
+    elif _year == thisYear - 2:
+        ret = ret + "三"
+    ret = ret + (["NULL","1","2","3","4","5","6","7","8","9","10","NULL","2","3","4","5","6","7"])[_class] #如果我没记错的话校徽是这样的
+    ret = ret + "班"
+
+    return ret
+
 def userLogin(userId, password):
     if isinstance(userId, int) and isinstance(password, int):
         DB.execute("SELECT * FROM user WHERE userId = %s AND password = %s;", (userId, password))
@@ -13,3 +51,5 @@ def userLogin(userId, password):
             return True, r
     else:
         return False, {"message": "请求接口错误"}
+
+def 
--- user.py
+++ user.py
@@ -1,46 +1,9 @@
 from flask import Blueprint, request, session
 
 import json
-import database as DB
+import oppressor as OP
 
 User = Blueprint('user', __name__)
-
-thisYear = 2020 # 以后要改成自动获取
-
-def classIdToString(a):
-    global thisYear
-    id = int(a)
-    _year = id // 100
-    _class = id % 100
-    ret = ""
-    # 特殊身份的判断
-    # 教师 100001 100002
-    # 管理员 110001 110002
-    # 系统 110003 110004
-    if _year//100 == 10:
-        ret = "教师"
-        return ret
-    elif _year//100 == 1:
-        ret = "管理员"
-        return ret
-    elif _year//100 == 12:
-        ret = "系统"
-        return ret
-    
-    if _class <= 10:
-        ret = ret + "高"
-    elif _class <= 17:
-        ret = ret + "蛟"
-    if _year == thisYear:
-        ret = ret + "一"
-    elif _year == thisYear - 1:
-        ret = ret + "二"
-    elif _year == thisYear - 2:
-        ret = ret + "三"
-    ret = ret + (["NULL","1","2","3","4","5","6","7","8","9","10","NULL","2","3","4","5","6","7"])[_class] #如果我没记错的话校徽是这样的
-    ret = ret + "班"
-
-    return ret
 
 @User.route('/user/login', methods=['POST'])
 def login():
@@ -50,27 +13,20 @@ def login():
     # 读取参数
     userid = json_data.get("userid")
     password = json_data.get("password")
-    DB.execute("SELECT * FROM user WHERE userId=%s AND password=%s;",(userid, password))
-    # 获取数据库返回的所有行
-    r = DB.fetchall()
-    if len(r) == 0:  # 如果没有对应的记录
-        respdata['message'] = "用户ID或密码错误！"
-    elif len(r) == 1:  # 如果只有一条记录说明符合要求
-        row = r[0]
-
+    st, val = OP.userLogin(userid, password)
+    if st:
         respdata['type'] = "SUCCESS"
         respdata['message'] = "登陆成功"
-        respdata['username'] = row[1]
-        respdata['class'] = row[2]
-        respdata['permission'] = row[3]
-        respdata['classname'] = classIdToString(row[2])
+        respdata['username'] = val[1]
+        respdata['class'] = val[2]
+        respdata['permission'] = val[3]
+        respdata['classname'] = OP.classIdToString(val[2])
 
         session['username'] = respdata['username']
         session['class'] = respdata['class']
         session['permission'] = respdata['permission']
-
-    elif len(r) > 1:  # 不然就是出现了两条一样的记录，此时为了安全考虑不能登录
-        respdata['message'] = "用户重复！请向管理员寻求帮助！"
+    else:
+        respdata.update(val)
     return json.dumps(respdata)  # 传回json数据
 
 @User.route('/user/logout', methods=['POST'])
