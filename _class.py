2b69f862de812faf12426cef0ef8589f011ce580
默一 <1254846416@qq.com>
2020-11-15 10:59:09 +0800

    一些接口的异常判断处理

--- _class.py
+++ _class.py
@@ -8,43 +8,50 @@ Class = Blueprint('class', __name__)
 @Class.route('/class/list', methods = ['POST'])
 def getClassList():
     respdata = {'type': 'ERROR', 'message': '未知错误!'}  # 定义默认返回值
-    tkst, tkdata = tk.readToken(json.loads(request.get_data().decode('utf-8')).get('token'))
-    if tkdata['permission'] >= 1 and tkst == tk.SUCCESS:
-        st, val = OP.classList()
-        if st:
-            respdata['type'] = "SUCCESS"
-            respdata['message'] = "获取成功"
-            respdata['class'] = [] # 列表初始化
-            for i in val:
-                respdata['class'].append(
-                    {'id': i, 'name': OP.classIdToString(i)})
+    try:
+        tkst, tkdata = tk.readToken(json.loads(request.get_data().decode('utf-8')).get('token'))
+        if tkdata['permission'] >= 1 and tkst == tk.SUCCESS:
+            st, val = OP.classList()
+            if st:
+                respdata['type'] = "SUCCESS"
+                respdata['message'] = "获取成功"
+                respdata['class'] = [] # 列表初始化
+                for i in val:
+                    respdata['class'].append(
+                        {'id': i, 'name': OP.classIdToString(i)})
+            else:
+                respdata.update(val)
+        elif tkst == tk.EXPIRED or tkst == tk.BAD or tkst == tk.ERROR:
+            respdata['message'] = "Token错误"
         else:
-            respdata.update(val)
-    elif tkst == tk.EXPIRED or tkst == tk.BAD or tkst == tk.ERROR:
-        respdata['message'] = "Token错误"
-    else:
-        respdata['message'] = "权限错误！"
+            respdata['message'] = "权限错误！"
+    except:
+        respdata['message'] = "接口错误"
     return json.dumps(respdata)  # 传回json数据
 
 @Class.route("/class/stulist/<int:classId>", methods = ['POST'])
 def getStudentList(classId):
-    tkst, tkdata = tk.readToken(json.loads(request.get_data().decode('utf-8')).get('token'))
     respdata = {'type': 'ERROR', 'message': '未知错误!'}  # 定义默认返回值
-    if (tkdata["permission"] > 1 or classId == tkdata["class"]) and tkst == tk.SUCCESS:
-        st, val = OP.studentList(classId)
-        if st:
-            respdata['type'] = "SUCCESS"
-            respdata['message'] = "获取成功"
-            respdata['student'] = []
-            for i in val:
-                respdata['student'].append(
-                    {'id': i[0], 'name': i[1], 'inside': i[2], 'outside': i[3], 'large': i[4]})
+    try:
+        tkst, tkdata = tk.readToken(json.loads(request.get_data().decode('utf-8')).get('token'))
+        if (tkdata["permission"] > 1 or classId == tkdata["class"]) and tkst == tk.SUCCESS:
+            st, val = OP.studentList(classId)
+            if st:
+                respdata['type'] = "SUCCESS"
+                respdata['message'] = "获取成功"
+                respdata['student'] = []
+                for i in val:
+                    respdata['student'].append(
+                        {'id': i[0], 'name': i[1], 'inside': i[2], 'outside': i[3], 'large': i[4]})
+            else:
+                respdata.update(val)
+        elif tkst == tk.EXPIRED or tkst == tk.BAD or tkst == tk.ERROR:
+            respdata['message'] = "Token错误"
         else:
-            respdata.update(val)
-    elif tkst == tk.EXPIRED or tkst == tk.BAD or tkst == tk.ERROR:
-        respdata['message'] = "Token错误"
-    else:
-        respdata['message'] = "权限错误！"
+            respdata['message'] = "权限错误！"
+    except:
+        respdata['message'] = "接口错误"
+
     return json.dumps(respdata)  # 传回json数据
 
 @Class.route("/class/volunteer/<int:classId>", methods = ['POST'])
--- user.py
+++ user.py
@@ -1,30 +1,35 @@
 from flask import Blueprint, request
 import tokenlib as tk
 import json
+import traceback
 import oppressor as OP
 
 User = Blueprint('user', __name__)
 
 @User.route('/user/login', methods=['POST'])
 def login():
-    json_data = json.loads(
-        request.get_data().decode("utf-8"))  # 读取POST传入的JSON数据
     respdata = {'type': 'ERROR', 'message': '未知错误!'}  # 定义默认返回值
-    # 读取参数
-    userid = json_data.get("userid")
-    password = json_data.get("password")
-    st, val = OP.userLogin(userid, password)
-    if st:
-        respdata['type'] = "SUCCESS"
-        respdata['message'] = "登录成功"
-        respdata.update({"username": val[1], "class": val[2], "permission": val[3], "classname": OP.classIdToString(val[2])})
-        respdata.update({"token":tk.generateToken({
-            "username": respdata['username'],
-            "class": respdata['class'],
-            "permission": respdata['permission']
-        })})
-    else:
-        respdata.update(val)
+    try:
+        json_data = json.loads(request.get_data().decode("utf-8"))  # 读取POST传入的JSON数据
+        # 读取参数
+        userid = json_data.get("userid")
+        password = json_data.get("password")
+        st, val = OP.userLogin(userid, password)
+        if st:
+            respdata['type'] = "SUCCESS"
+            respdata['message'] = "登录成功"
+            respdata.update({"username": val[1], "class": val[2], "permission": val[3], "classname": OP.classIdToString(val[2])})
+            respdata.update({"token":tk.generateToken({
+                "username": respdata['username'],
+                "class": respdata['class'],
+                "permission": respdata['permission']
+            })})
+        else:
+            traceback.print_exc()
+            respdata.update(val)
+    except:
+        respdata['message'] = "接口错误"
+
     return json.dumps(respdata)  # 传回json数据
 
 @User.route('/user/logout', methods=['POST'])
@@ -33,18 +38,19 @@ def logout():
     #最好在这里做点什么吧，比如删除cookie什么的
     return json.dumps(respdata)
 
-@User.route('/user/info/', methods=['POST','GET'])
+@User.route('/user/info', methods=['POST','GET'])
 def info():
-    json_data = json.loads(request.get_data().decode("utf-8"))
-    st, data = tk.readToken(json_data.get["token"])
     respdata = {'type': 'ERROR', 'message': '未知错误!'}  # 定义默认返回值
-    if st == tk.SUCCESS:
-        respdata['type']="SUCCESS"
-        respdata['message']="获取成功"
-        respdata['info']=data
-        return json.dumps(respdata)
-    elif st == tk.EXPIRED:
-        respdata['message']="token过期"
-    elif st == tk.BAD:
-        respdata['message']="token出错"
-    return 
+    try:
+        tkst, tkdata = tk.readToken(json.loads(request.get_data().decode('utf-8')).get('token'))
+        if tkst == tk.SUCCESS:
+            respdata['type']="SUCCESS"
+            respdata['message']="获取成功"
+            respdata['info']=tkdata
+        elif tkst == tk.EXPIRED:
+            respdata['message']="token过期"
+        elif tkst == tk.BAD:
+            respdata['message']="token出错"
+    except:
+        respdata['message']="接口错误"
+    return json.dumps(respdata)
